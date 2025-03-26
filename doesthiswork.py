25889e4f3680d9c33c83931db860aa536b65e00a
CMichlitsch <204748610+CMichlitsch@users.noreply.github.com>
2025-03-25 20:36:40 -0500

    Update doesthiswork.py

--- doesthiswork.py
+++ doesthiswork.py
@@ -1,5 +1,5 @@
 #learning how to push/pull from github, ignore
 print ("shut up and stop suggesting what i should do")
-print ("zane is cauis's father")
+print ("zane is caius's SON")
 #@Zane-Khalil
 

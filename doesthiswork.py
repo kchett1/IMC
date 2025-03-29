53d3997ef04ede6e08864da1b323296bf1503c05
Zane-Khalil <zsk5097@psu.edu>
2025-03-28 21:03:21 -0400

    Update doesthiswork.py

--- doesthiswork.py
+++ doesthiswork.py
@@ -1,5 +1,5 @@
 #learning how to push/pull from github, ignore
 print ("shut up and stop suggesting what i should do")
-print ("zane is caius's SON")
+print ("good boy caius, good boy")
 #@Zane-Khalil
 

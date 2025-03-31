0b96f7d5926c04f5322e2ee17242f621a0df0ba9
CMichlitsch <204748610+CMichlitsch@users.noreply.github.com>
2025-03-30 22:20:42 -0500

    dw abt it

--- doesthiswork.py
+++ doesthiswork.py
@@ -1,5 +1,5 @@
 #learning how to push/pull from github, ignore
 print ("shut up and stop suggesting what i should do")
-print ("good boy caius, good boy")
+print ("good boy zane, good boy")
 #@Zane-Khalil
 

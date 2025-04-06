4dda157bd26a9a2ca0d67f8904c8de11c41b3606
Zane-Khalil <zsk5097@psu.edu>
2025-04-06 18:57:22 -0400

    Update doesthiswork.py

--- doesthiswork.py
+++ doesthiswork.py
@@ -1,5 +1,5 @@
 #learning how to push/pull from github, ignore
 print ("shut up and stop suggesting what i should do")
-print ("good boy zane, good boy")
+print ("good boy cauis, good boy")
 #@Zane-Khalil
 
